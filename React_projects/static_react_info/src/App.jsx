d5fc3e77fd2c7e215d08a9c6756c3b31d7de9cf6
Stan Loosmore <Loosmore@usc.edu>
2023-11-11 20:51:53 -0800

    First few react projects

---
+++ React_projects/static_react_info/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Content from "./content.jsx";
+import Nav from "./nav.jsx";
+//import Footer from "./footer/footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Content />
+    </>
+  );
+}
+
+export default App;
---
+++ React_projects/static_react_info/src/footer/footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <small>© 2023 transverse. All rights reserved.</small>
+    </footer>
+  );
+}
+
+export default Footer;
